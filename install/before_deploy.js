06920f7b247f861e25c761b5da4c9a1a4615e629
mscho <martin@f-schophaus.de>
2015-07-23 20:28:54 +0200

    catch exceptions in before_deploy.js

--- install/before_deploy.js
+++ install/before_deploy.js
@@ -1,3 +1,9 @@
 var installation = require('./installation.js');
 installation.compileSCSS();
 installation.createDatabaseJson();
+
+process.on('uncaughtException', function(err) {
+    console.log("uncaughtException");
+    console.log(err.message);
+    console.log(err.stack);
+});
