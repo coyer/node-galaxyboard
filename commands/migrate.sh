777e31b59ee264e423a0dcd0f02635dbf099cfdd
mscho <martin@f-schophaus.de>
2015-07-25 13:47:03 +0200

    changed deployment provider

--- commands/migrate.sh
+++ commands/migrate.sh
@@ -1,4 +1,4 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 nodejs $SCRIPTPATH/create_database_json.js
-$SCRIPTPATH/../node_modules/.bin/db-migrate up
+nodejs $SCRIPTPATH/../node_modules/.bin/db-migrate up
---
+++ deploy.sh
@@ -0,0 +1,8 @@
+cat ./known_hosts > ~/.ssh/known_hosts
+cat ~/.ssh/known_hosts
+eval `ssh-agent -s`
+ssh-add ./id_rsa
+ssh galaxyboard@euve58546.serverprofi24.de -p 55123 'sh ~/galaxyboard/commands/down.sh'
+rsync -avze 'ssh -p 55123' ./ galaxyboard@euve58546.serverprofi24.de:galaxyboard
+ssh galaxyboard@euve58546.serverprofi24.de -p 55123 'sh ~/galaxyboard/commands/migrate.sh'
+ssh galaxyboard@euve58546.serverprofi24.de -p 55123 'sh ~/galaxyboard/commands/up.sh'
