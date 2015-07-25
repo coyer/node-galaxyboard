c6702a787ae094537bef4391556b285605be06f7
mscho <martin@f-schophaus.de>
2015-07-25 14:28:22 +0200

    changed deployment provider

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 cat ./known_hosts > ~/.ssh/known_hosts
-cat ~/.ssh/known_hosts
 eval `ssh-agent -s`
 ssh-add ./id_rsa
 ssh galaxyboard@euve58546.serverprofi24.de -p 55123 'sh ~/galaxyboard/commands/down.sh'
