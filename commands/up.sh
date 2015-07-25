b4b4584ba36430a6328a2558e95b6eb5775a369d
mscho <martin@f-schophaus.de>
2015-07-25 13:02:04 +0200

    fixed up.sh error

--- commands/up.sh
+++ commands/up.sh
@@ -1,8 +1,8 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 if [ -f ./pid ]; then
+    echo "Board already running. If not delete ./pid file in nodeboard/commands";
+else
     nodejs $SCRIPTPATH/../board.js &
     echo $! > $SCRIPTPATH/pid
-else
-    echo "Board already running. If not delete ./pid file in nodeboard/commands";
 fi
