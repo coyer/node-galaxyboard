ea0558ab9a2bdadff48af9defe8ebd9dcdc60067
mscho <martin@f-schophaus.de>
2015-07-25 13:23:13 +0200

    fixed installation

--- commands/installation.js
+++ commands/installation.js
@@ -1,28 +1,10 @@
 var fs = require('fs');
-
-var isCloudControl = function() {
-    return typeof process.env.CRED_FILE !== 'undefined';
-};
-
-var getCredentialFilePath = function() {
-    return isCloudControl() ? process.env.CRED_FILE : __dirname + '/../credentials.json';
-};
-
-exports.getCredentials = function() {
-    return require(getCredentialFilePath())
-};
+var config = require(process.env.GALAXYBOARD_CONFIG);
 
 exports.createDatabaseJson = function() {
-    var credentials = exports.getCredentials();
-    var dbType = typeof credentials.MYSQLS === 'undefined' ? 'MYSQLD' : 'MYSQLS';
-    var obj = {
-        host: credentials[dbType][dbType + '_HOSTNAME'],
-        database: credentials[dbType][dbType + '_DATABASE'],
-        user: credentials[dbType][dbType + '_USERNAME'],
-        password: credentials[dbType][dbType + '_PASSWORD'],
-        driver: "mysql",
-        multipleStatements: true
-    };
+    var obj = config.db;
+    obj.multipleStatements = true;
+    obj.driver = 'mysql';
     var result = { dev: obj, other: obj };
     fs.writeFileSync(__dirname + '/../database.json', JSON.stringify(result));
 };
